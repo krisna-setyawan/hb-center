6ec074f80025e04c6c855c3e738922a7c3c127f5
Krisna Setyawan <krisn4setyawan@gmail.com>
2023-06-17 14:19:17 +0700

    get pemesanan from api haebot's order (1)

--- app/Controllers/Sales/Order.php
+++ app/Controllers/Sales/Order.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Sales;
 
 use App\Models\Sales\PenjualanOrderModel;
 use CodeIgniter\RESTful\ResourcePresenter;
+use CodeIgniter\Config\Services;
 
 class Order extends ResourcePresenter
 {
@@ -19,5 +20,35 @@ class Order extends ResourcePresenter
         ];
 
         return view('sales/order/index', $data);
+    }
+
+    public function show($no_pemesanan = null, $id_perusahaan = null)
+    {
+        $client = Services::curlrequest();
+
+        // Get data perusahaan
+        $url_get_perusahaan = $_ENV['URL_API'] . 'public/get-perusahaan/' . $id_perusahaan;
+        $response_get_perusahaan = $client->request('GET', $url_get_perusahaan);
+        $status = $response_get_perusahaan->getStatusCode();
+        $responseJson = $response_get_perusahaan->getBody();
+        $responseArray = json_decode($responseJson, true);
+        $perusahaan = $responseArray['data_perusahaan'][0];
+
+        // Get data pemesanan
+        $url_get_pemesanan = $perusahaan['url'] . 'public/hbapi-get-detail-pemesanan/' . $no_pemesanan;
+        $response_get_pemesanan = $client->request('GET', $url_get_pemesanan);
+        $status = $response_get_pemesanan->getStatusCode();
+        $responseJsonPemesanan = $response_get_pemesanan->getBody();
+        $responseArrayPemesanan = json_decode($responseJsonPemesanan, true);
+
+        dd($responseArrayPemesanan);
+
+        // $data = [];
+
+        // $json = [
+        //     'data' => view('purchase/pemesanan/show', $data),
+        // ];
+
+        // echo json_encode($json);
     }
 }
